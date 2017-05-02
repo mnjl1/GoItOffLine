de03e58c37e3737a9889812b366068bd589265d8
mnjl <d.manzhula@gmail.com>
2017-05-02 19:42:18 +0300

    ver 1.0

---
+++ src/Offline1_Magic_Square/MagicSquareTest.java
@@ -0,0 +1,26 @@
+package Offline1_Magic_Square;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by Dmytro on 02.05.2017.
+ */
+public class MagicSquareTest {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Enter size1");
+
+
+        Square newSquare = new Square();
+
+        newSquare.addSize();
+
+        newSquare.fillArray();
+
+
+
+    }
+}
---
+++ src/Offline1_Magic_Square/Square.java
@@ -0,0 +1,64 @@
+package Offline1_Magic_Square;
+
+import java.util.Scanner;
+
+/**
+ * Created by Dmytro on 02.05.2017.
+ */
+
+
+public class Square {
+    private int[][] squareArray;
+    int sizeArray1;
+    int sizeArray2;
+
+    Scanner scanner = new Scanner(System.in);
+
+    public Square() {
+
+        this.sizeArray1 = sizeArray1;
+        this.sizeArray2 = sizeArray2;
+        this.squareArray = new int[sizeArray1][sizeArray2];
+    }
+
+    public int[][] getSquareArray() {
+        return squareArray;
+    }
+
+    public void setSquareArray(int[][] squareArray) {
+        this.squareArray = squareArray;
+    }
+
+    public void addSize() {
+        System.out.println("Enter array size1.");
+        sizeArray1 = scanner.nextInt();
+
+        System.out.println("Enter array size2");
+        sizeArray2 = scanner.nextInt();
+    }
+
+    public void fillArray() {
+
+        for (int i = 0; i < sizeArray1; i++) {
+            for (int j = 0; j < sizeArray2; j++) {
+
+                System.out.println("Enter next element:");
+                squareArray[i][j] = scanner.nextInt();
+
+            }
+
+
+        }
+
+        System.out.println("New array: ");
+
+        for(int i[]: squareArray) {
+            for (int j: i) {
+                System.out.println(j +"\t");
+            }
+            System.out.println("\n");
+        }
+
+    }
+
+}
